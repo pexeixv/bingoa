6713b04d3442e39352ad37b12a485363dfb2e964
Gavin Pereira <68476117+pexeixv@users.noreply.github.com>
2022-07-21 07:01:08 +0530

    v1 v1

---
+++ README.md
@@ -0,0 +1,12 @@
+# ![BinGoa Events](./public/wordmark.png)
+
+![Website Preview](./public/preview.png)
+
+## Link
+
+[bingoa.in](https://bingoa.in)
+
+## Tech stack
+
+- [Astro.js](https://astro.build/)
+- [TailwindCSS](https://tailwindcss.com/)
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
+  theme: {
+    fontFamily: {
+      serif: [
+        "Playfair Display",
+        "ui-serif",
+        "Georgia",
+        "Cambria",
+        "Times New Roman",
+        "Times",
+        "serif",
+      ],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
